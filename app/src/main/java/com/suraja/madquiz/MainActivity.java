f7535cde0401717937371d326203321cdad2d5f3
Suraja Baral <nanusuru07@gmail.com>
2023-01-25 20:14:52 +0545

    files added

---
+++ app/src/main/java/com/suraja/madquiz/MainActivity.java
@@ -0,0 +1,107 @@
+package com.suraja.madquiz;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+
+    TextView tvTotalques;
+    TextView tvQues;
+    Button btnAnsA, btnAnsB, btnAnsC;
+    Button btnSubmit;
+
+    int score =0;
+    int totalQuestion = QuestionAnswer.question.length;
+    int currentQuestionIndex=0;
+    String selectedAnswer = "";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tvTotalques = findViewById(R.id.tvTotalques);
+        tvQues = findViewById(R.id.tvQues);
+        btnAnsA = findViewById(R.id.btnAnsA);
+        btnAnsB = findViewById(R.id.btnAnsB);
+        btnAnsC = findViewById(R.id.btnAnsC);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnAnsA.setOnClickListener(this);
+        btnAnsB.setOnClickListener(this);
+        btnAnsC.setOnClickListener(this);
+        btnSubmit.setOnClickListener(this);
+
+        tvTotalques.setText(" Total Questions: "+ totalQuestion);
+        
+        loadNewQuestion();
+    }
+
+
+
+    @Override
+    public void onClick(View v) {
+
+        btnAnsA.setBackgroundColor(Color.BLACK);
+        btnAnsB.setBackgroundColor(Color.BLACK);
+        btnAnsC.setBackgroundColor(Color.BLACK);
+
+        Button clickedButton= (Button) v;
+        if(clickedButton.getId()== R.id.btnSubmit){
+            if(selectedAnswer.equals(QuestionAnswer.correctAnswers[currentQuestionIndex])){
+                score++;
+            }
+            currentQuestionIndex++;
+            loadNewQuestion();
+
+
+        }else{
+            //choices button clicked
+            selectedAnswer= clickedButton.getText().toString();
+            clickedButton.setBackgroundColor(Color.CYAN);
+        }
+
+    }
+
+    void loadNewQuestion() {
+        if(currentQuestionIndex == totalQuestion){
+            finishQuiz();
+            return;
+        }
+
+        tvQues.setText(QuestionAnswer.question[currentQuestionIndex]);
+        btnAnsA.setText(QuestionAnswer.choices[currentQuestionIndex][0]);
+        btnAnsB.setText(QuestionAnswer.choices[currentQuestionIndex][1]);
+        btnAnsC.setText(QuestionAnswer.choices[currentQuestionIndex][2]);
+    }
+
+    void finishQuiz() {
+        String passStatus = "";
+        if (score > totalQuestion*0.60){
+            passStatus = "You Won";
+        }else{
+            passStatus= "Try Again";
+        }
+
+        new AlertDialog.Builder(this)
+                .setTitle(passStatus)
+                .setMessage("Score is "+ score+" out of "+totalQuestion)
+                .setPositiveButton("Restart", (dialogInterface, i) -> restartQuiz())
+                .setCancelable(false)
+                .show();
+    }
+
+    void restartQuiz() {
+        score = 0;
+        currentQuestionIndex = 0;
+        loadNewQuestion();
+
+    }
+}
---
+++ app/src/main/java/com/suraja/madquiz/QuestionAnswer.java
@@ -0,0 +1,21 @@
+package com.suraja.madquiz;
+
+public class QuestionAnswer {
+    public static  String question[]={
+            "Which one is the even number?",
+            "Which one is the odd number?",
+            "Which one is the multiple of 2?",
+    };
+
+    public static String choices[][]={
+            {"1","2","3"},
+            {"1","2","6"},
+            {"1", "4", "3"}
+    };
+
+    public static String correctAnswers[]={
+                "2,",
+                "1",
+                "4"
+    };
+}
