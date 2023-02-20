d058f4b5620f0e21acb2916fc3c34afbddf6e646
Suraja Baral <nanusuru07@gmail.com>
2023-02-20 12:29:38 +0545

    SplashScreen

--- app/src/main/java/com/suraja/madquiz/QuestionAnswer.java
+++ app/src/main/java/com/suraja/madquiz/QuestionAnswer.java
@@ -2,20 +2,41 @@ package com.suraja.madquiz;
 
 public class QuestionAnswer {
     public static  String question[]={
-            "Which one is the even number?",
-            "Which one is the odd number?",
-            "Which one is the multiple of 2?",
+            "Who is the Prime Minster of Nepal?",
+            "in the creation code, TRIPPLE is written as SQHOOKD. How is DISPOSE written in?",
+            "If '+' means '-', '-' means '*', '/' means '+', and '*' means '/', then 10*5/3-2+3=?",
+            "The United States .... going to conduct elections soon.",
+            "Fear of Travelling?",
+            "Which one is the synonym of CURRY?",
+            "The antonym of the word RELINQUISH is?",
+            "How many total squares are there on a standard chess board?",
+            "Take 2000. Now add 140 to it. Take away 30, and add another 80. Now add 440. Finally take away 16. What's your final total?",
+            "When counting from 1-100, how many times will you come across the number 7?"
     };
 
     public static String choices[][]={
-            {"1","2","3"},
-            {"1","2","6"},
-            {"1", "4", "3"}
+            {"Pushpa Kamal Dahal","Sher Bahadur Deuba","Khadga Prasad Sharma Oli"},
+            {"ESJTPTF","DSOPSID","CHRONRD"},
+            {"7", "23/3", "5"},
+            {"are", "have", "is"},
+            {"Atephobia", "Hodophobia", "Koniophobia"},
+            {"Spicy", "Flatter", "Squander"},
+            {"Possess", "Deny", "Renounce"},
+            {"64", "204", "128"},
+            {"2514", "2644", "2614"},
+            {"19", "20","21"}
     };
 
     public static String correctAnswers[]={
-                "2,",
-                "1",
-                "4"
+                "Pushpa Kamal Dahal",
+                "CHRONRD",
+                "5",
+                "is",
+                "Hodophobia",
+                 "Flatter",
+                "Possess",
+                "204",
+                "2614",
+                "20"
     };
 }
